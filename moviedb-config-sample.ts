5bffb55d8668c1fa3fe94276d3bce7d8ece535cd
diogospaiva <dsoarespaiva@gmail.com>
2020-04-06 08:15:18 -0300

    Configuração do moviedb

---
+++ moviedb-config-sample.ts
@@ -0,0 +1,8 @@
+// The Movie Database Api:
+// Para preencher o campo de filmes será obrigatório o uso da api https://www.themoviedb.org/.
+// Você precisará se cadastrar para conseguir uma chave de acesso.
+
+export const SERVER_API_URL = 'https://api.themoviedb.org/';
+
+// COLE AQUI SEU Token de Leitura da API (v4 auth)/
+export const MOVIEDB = 'COLE AQUI';
